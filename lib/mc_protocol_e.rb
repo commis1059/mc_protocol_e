bd336926d8ea0ac9dd48558a4fd5d51851558004
commis1059 <ommis.1059@gmail.com>
2019-08-29 15:00:14 +0900

    Add required files

--- lib/mc_protocol_e.rb
+++ lib/mc_protocol_e.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
+require 'mc_protocol_e/frame_1e/access_route'
+require 'mc_protocol_e/frame_1e/batch_read_in_word'
+require 'mc_protocol_e/frame_1e/batch_write_in_word'
+require 'mc_protocol_e/frame_1e/device_range'
+require 'mc_protocol_e/frame_1e/request'
+require 'mc_protocol_e/frame_1e/response'
+require 'mc_protocol_e/frame_3e/access_route'
+require 'mc_protocol_e/frame_3e/batch_read_in_word'
+require 'mc_protocol_e/frame_3e/batch_write_in_word'
+require 'mc_protocol_e/frame_3e/device_range'
+require 'mc_protocol_e/frame_3e/error_info'
+require 'mc_protocol_e/frame_3e/request'
+require 'mc_protocol_e/frame_3e/response'
 require "mc_protocol_e/client"
 require "mc_protocol_e/version"
